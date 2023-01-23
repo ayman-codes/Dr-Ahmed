763a72d76c93a9b23e57ad5abb230adeee59267e
ayman-code <ayman.ab.work@gmail.com>
2023-01-23 12:40:41 -0800

    commit3

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 <h1 style='background:#0BF7FE; color:black;'><center>Dr-Ahmed Portfolio</center></h1> 
 
 This is a repository made for Dr-Ahmed portfolio website
-
--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+from io import BytesIO
 from pathlib import Path
+import requests
 import streamlit as st
 from PIL import Image
 
 # Path settings
 current_dir = Path(__file__).parent if '__file__' in locals() else Path.cwd()
 css_file = current_dir / 'styles'/'main.css'
-pfp = current_dir / 'assets/'/'Pictures'/'pfp2.png'
+pfp = current_dir / 'assets'/'Images'/'pfp2.png'
 resume_file = current_dir / "assets" /'CV'/ "cv3b.pdf"
 
 # --- GENERAL SETTINGS ---
@@ -13,19 +15,11 @@ PAGE_TITLE = "Digital CV | Dr. Ahmed Ayman Obidou"
 PAGE_ICON = ":wave:"
 NAME = "Dr. Ahmed Ayman Obidou"
 DESCRIPTION = """
-I'm a highly experienced and dedicated family doctor from Egypt. I have a bachelor's degree in 
-Medicine from Algeria and a specialist degree in Family Medicine from Syria.
-I can speak French, Arabic, and English, and have over 17 years of experience working in the medical 
-field - both as a Medical Director and Family Doctor. I'm well-versed in providing patient centered care
-and has implemented quality improvement initiatives to improve patient access to care.
-I've experience in leading projects to improve patient access to care and providing health 
-education and counseling to patients and families. I'm committed to providing the highest quality 
-of care to my patients. I'm passionate about my work, as it allows to help more people and give people
-the medical care that they need. I'm constantly striving to be a better version of myself.
+Family doctor specialist, 17 years experience in family medicine and Medical director.
 """
 EMAIL = "ahmadabido2010@hotmail.com"
 SOCIAL_MEDIA = {
-    "LinkedIn": "https://www.linkedin.com/in/ahmadayman-abido-91a647211",
+    "LinkedIn ": "https://www.linkedin.com/in/ahmadayman-abido-91a647211",
     "Instagram": "https://instagram.com/ahmadaymanabido?igshid=OGQ2MjdiOTE=",
 }
 CERTIFICATES = {
@@ -41,19 +35,15 @@ st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 # --- LOAD CSS, PDF & PROFIL PIC ---
 with open(css_file) as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+    
 with open(pfp, "rb") as pdf_file:
     PDFbyte = pdf_file.read()
 profile_pic = Image.open(pfp)
 
-
-pfp = "https://raw.githubusercontent.com/ayman-codes/Deep_Learning/main/Project_final/UI/images/icon.png"
-response = requests.get(url_icon)
-img = Image.open(BytesIO(response.content))
-
 # hero section
 col1, col2 = st.columns(2, gap='small')
 with col1:
-    st.image(pfp, width=230)
+    st.image(profile_pic, width=230)
 
 with col2:
     st.title(NAME)
@@ -65,3 +55,61 @@ with col2:
         mime="application/octet-stream",
     )
     st.write("📫", EMAIL)
+    
+# --- SOCIAL LINKS ---
+st.write('\n')
+cols = st.columns(len(SOCIAL_MEDIA))
+for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
+    cols[index].write(f"[{platform}]({link})")
+    
+# About me
+st.write('#')
+st.subheader('About me')
+st.write(
+    '''
+    I'm a highly experienced and dedicated family doctor from Egypt. I have a bachelor's degree in 
+    Medicine from Algeria and a specialist degree in Family Medicine from Syria.
+    I can speak French, Arabic, and English, and have over 17 years of experience working in the medical 
+    field - both as a Medical Director and Family Doctor. I'm well-versed in providing patient centered care
+    and has implemented quality improvement initiatives to improve patient access to care.
+    I've experience in leading projects to improve patient access to care and providing health 
+    education and counseling to patients and families. I'm committed to providing the highest quality 
+    of care to my patients. I'm passionate about my work, as it allows to help more people and give people
+    the medical care that they need. I'm constantly striving to be a better version of myself.
+    '''
+)
+
+# Experience
+st.write('#')
+st.subheader('Experience in Family Medicine')
+st.write(
+    '''
+    - Delivered quality, patient-centered primary care 
+    - Supervised medical staff, conducted performance reviews 
+    - Developed and implemented quality improvement initiatives 
+    - Researched and presented best practices to staff 
+    - Coordinated patient care with specialists, hospitals, and community resources 
+    - Led projects to improve patient access to care 
+    - Provided health education and counseling to patients and families 
+    - Conducted physical exams, diagnosis and treatments.
+
+    '''
+)
+
+# Experience
+st.write('#')
+st.subheader('Experience as a Medical director')
+st.write(
+    '''
+    - ✔️ Oversee operations, staff, and patient care 
+    - ✔️ Develop and implement best practices for operations and patient care 
+    - ✔️ Serve as primary contact, communication, and decision-maker for staff, patients, and community
+    '''
+)
+
+# --- Projects & Accomplishments ---
+st.write('\n\n\n')
+st.subheader("Projects & Accomplishments")
+st.write("---")
+for project, link in CERTIFICATES.items():
+    st.write(f"[{project}]({link})")
