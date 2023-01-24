a5e7e3c957183f6e8590cfeffe2810e98d50e7a2
ayman-code <ayman.ab.work@gmail.com>
2023-01-24 05:34:20 -0800

    xc

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-# LOAD CSS
-css = 'https://raw.githubusercontent.com/ayman-codes/Dr-Ahmed/main/styles/main.css?token=GHSAT0AAAAAAB5ORJB4BMPALOMCLU22Y3Q2Y6P2AYA'
-# --- LOAD CSS from github ---
-response_css = requests.get(css)
-print(response_css.text)
